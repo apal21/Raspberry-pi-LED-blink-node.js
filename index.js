01742140d81f3de1fb08904c171fd263551f3b36
Apal Shah <apal21@users.noreply.github.com>
2018-02-10 23:28:09 +0530

    Added Comments

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const Gpio = require('onoff').Gpio
-let led = new Gpio(4, 'out')
-
-led.writeSync(1)
+let led = new Gpio(4, 'out') // GPIO4 (pin #7)
 
 on()
 
+// Turn on LED
 function on() {
     led.writeSync(1)
     console.log('ON');
@@ -13,6 +12,7 @@ function on() {
     }, 2000)
 }
 
+// Turn off LED
 function off() {
     led.writeSync(0)
     console.log('OFF');
